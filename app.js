c424c3020c1fdf738fc9b4208cb7f22162444e64
Neru <nenriquev1@gmail.com>
2022-08-18 22:26:02 +0200

    Subiendo proyecto

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import path from 'path'
+
+
+const app = express()
+
+app.listen(3000, () =>{
+  console.log("Servidor corriendo en el puerto 3000")
+})
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('./views/index.html'))
+})
+
+app.get('/product_detail', (req, res) => {
+  res.sendFile(path.resolve('./views/product_detail.html'))
+})
