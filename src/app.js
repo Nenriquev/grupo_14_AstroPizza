68430fabd0e6e5012e4055434bcff12639acd1cf
RamiroDAgnone <ramiro.dagnone@gmail.com>
2022-09-15 14:44:38 -0300

    data product detail agregado

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const indexRouter = require('./routers/index.js');
 const productRouter = require('./routers/products.js');
 const cartRouter = require('./routers/cart.js');
-
+const methodOverride = require("method-override");
 
 const app = express()
 
@@ -12,11 +12,13 @@ app.listen(3000, () =>{
   console.log("Servidor corriendo en el puerto 3000")
 })
 
+/* CONFIGURACÍON */
 app.set('views', path.join(__dirname + '/views'));
 app.set('view engine', 'ejs');
-
+app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, '../public')));
-
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 /* INDEX */
 app.use('/', indexRouter)
--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,8 +1,49 @@
-const path = require('path')
+const path = require('path');
+const fs = require("fs");
+
+function findAllQuesos(){
+  const jsonData = fs.readFileSync(path.join(__dirname, "../data/product_detail_data/extras_quesos.json"))
+  const data = JSON.parse(jsonData)
+  return data
+}
+
+function findAllVegetales(){
+  const jsonData = fs.readFileSync(path.join(__dirname, "../data/product_detail_data/extras_vegetales.json"))
+  const data = JSON.parse(jsonData)
+  return data
+}
+
+function findAllProteinas(){
+  const jsonData = fs.readFileSync(path.join(__dirname, "../data/product_detail_data/extras_proteinas.json"))
+  const data = JSON.parse(jsonData)
+  return data
+}
+
+function findAllBebida(){
+  const jsonData = fs.readFileSync(path.join(__dirname, "../data/product_detail_data/extras_bebidas.json"))
+  const data = JSON.parse(jsonData)
+  return data
+}
+
+function findAllCerveza(){
+  const jsonData = fs.readFileSync(path.join(__dirname, "../data/product_detail_data/extras_cervezas.json"))
+  const data = JSON.parse(jsonData)
+  return data
+}
 
 const productController = {
   products: (req, res) => {
-    res.render('product_detail.ejs')
+    const dataQuesos = findAllQuesos();
+    const dataVegetales = findAllVegetales();
+    const dataProteinas = findAllProteinas();
+    const dataBebida = findAllBebida();
+    const dataCerveza = findAllCerveza();
+
+    res.render('product_detail.ejs', { dataQuesos: dataQuesos, 
+                                       dataVegetales: dataVegetales, 
+                                       dataProteinas: dataProteinas,
+                                       dataBebida: dataBebida,
+                                       dataCerveza: dataCerveza})
   },
 
 }
