bf3abcab2ff6fccfff1ba7aacd7a7e5258da6abb
matirene <matirene1@hotmail.com>
2022-10-31 21:23:01 -0300

    localmiddle

--- src/middlewares/localsMiddle.js
+++ src/middlewares/localsMiddle.js
@@ -1,5 +1,5 @@
 function localsMiddleware (req, res, next){
-    res.locals.usarios = null
+    res.locals.usuario = null
 
     if(req.session.usuarioLogueado){
         res.locals.usuario = req.session.usuarioLogueado;
