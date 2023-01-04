0b75c3954827990430279347051d58308d663650
RamiroDAgnone <ramiro.dagnone@gmail.com>
2023-01-04 16:28:20 -0300

    API users added functionality

--- src/routers/api/usersRoutes.js
+++ src/routers/api/usersRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const apiUsersController = require('../../controllers/api/usersController');
 
 router.get('/', apiUsersController.redirect);
+router.get('/list', apiUsersController.allUsers);
 router.get('/page/:id', apiUsersController.list);
 router.get('/:id', apiUsersController.detail);
 
